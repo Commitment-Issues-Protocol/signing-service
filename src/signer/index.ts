f9f20d0617d827739352e64de3ccd3baa1c05b35
Leah <lleahhhh@proton.me>
2026-07-25 21:37:25 +0000

    added key & data parameters

--- src/signer/index.ts
+++ src/signer/index.ts
@@ -1,6 +1,12 @@
 import { sign as cryptoSign } from 'node:crypto';
+
 import type { SigningKey } from './ssh-key.ts';
 
+/**
+ *
+ * @param key
+ * @param data
+ */
 export function sign(key: SigningKey, data: Buffer): Buffer {
   return cryptoSign(null, data, key.privateKey);
 }
